b50167d585861c20791f537d84f53eb62a3f1dca
Andre McCurdy <armccurdy@gmail.com>
2023-01-18 23:37:34 -0800

    check fread() return values to avoid compiler warnings
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- src/rbus/rbus_config.c
+++ src/rbus/rbus_config.c
@@ -95,8 +95,8 @@ int rbusConfig_ReadGetTimeout()
     {
         fp = fopen(RBUS_GET_TIMEOUT_OVERRIDE, "r");
         if(fp != NULL) {
-            fread(buf, 1, sizeof(buf), fp);
-            timeout = atoi(buf);
+            if (fread(buf, 1, sizeof(buf), fp) > 0)
+                timeout = atoi(buf);
             fclose(fp);
         }
         if (timeout > 0)
@@ -116,8 +116,8 @@ int rbusConfig_ReadSetTimeout()
     {
         fp = fopen(RBUS_SET_TIMEOUT_OVERRIDE, "r");
         if(fp != NULL) {
-            fread(buf, 1, sizeof(buf), fp);
-            timeout = atoi(buf);
+            if (fread(buf, 1, sizeof(buf), fp) > 0)
+                timeout = atoi(buf);
             fclose(fp);
         }
         if (timeout > 0)
